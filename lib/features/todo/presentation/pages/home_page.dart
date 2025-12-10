379f8935de1bbca3c20968882acc730dd7e29306
christ oderic <christyassinguezo@gmail.com>
2025-12-10 20:01:13 +0100

    list, calendar, kanban ok

--- lib/features/todo/presentation/pages/home_page.dart
+++ lib/features/todo/presentation/pages/home_page.dart
@@ -12,123 +12,190 @@ class HomePage extends StatelessWidget {
     return Theme(
       data: AppTheme.lightTheme,
       child: Scaffold(
-        body: Container(
-          height: 230,
-          color: AppColors.secondary,
-          child: Stack(
-            children: [
-              Positioned(
-                bottom: -55,
-                left: -55,
-                child: Container(
-                  width: 200,
-                  height: 200,
-                  decoration: const BoxDecoration(
-                    color: AppColors.primary,
-                    shape: BoxShape.circle,
-                  ),
-                ),
-              ),
-              Positioned(
-                bottom: 70,
-                left: 10,
-                child: Align(
-                  alignment: Alignment(-0.25, -0.5),
-                  child: IconButton(
-                    icon: const Icon(
-                      Symbols.person,
-                      size: 40,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {},
-                  ),
-                ),
-              ),
-              Positioned(
-                bottom: 45,
-                left: 10,
-                right: 0,
-                child: Text(
-                  "Today - ${DateTime.now().day} ${_monthName(DateTime.now().month)}",
-                  style: AppTypography.body.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Positioned(
-                bottom: 15,
-                left: 10,
-                right: 0,
-                child: Text(
-                  "Dashboard",
-                  style: AppTypography.titre3.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 85,
-                right: 15,
-                child: Container(
-                  width: 60,
-                  height: 60,
-                  decoration: const BoxDecoration(
-                    color: AppColors.primary,
-                    shape: BoxShape.circle,
-                  ),
-                  child: IconButton(
-                    icon: const Icon(
-                      Symbols.settings,
-                      size: 40,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {},
-                  ),
-                ),
-              ),
-              Positioned.fill(
-                child: Align(
-                  alignment: Alignment.centerRight,
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 90),
+        body: Column(
+          children: [
+            Container(
+              height: 230,
+              color: AppColors.secondary,
+              child: Stack(
+                children: [
+                  Positioned(
+                    bottom: -55,
+                    left: -55,
                     child: Container(
-                      height: 35,
-                      width: 240,
-                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      width: 200,
+                      height: 200,
+                      decoration: const BoxDecoration(
+                        color: AppColors.primary,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 70,
+                    left: 10,
+                    child: Align(
+                      alignment: Alignment(-0.25, -0.5),
+                      child: IconButton(
+                        icon: const Icon(
+                          Symbols.person,
+                          size: 40,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 45,
+                    left: 10,
+                    right: 0,
+                    child: Text(
+                      "Today - ${DateTime.now().day} ${_monthName(DateTime.now().month)}",
+                      style: AppTypography.body.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 15,
+                    left: 10,
+                    right: 0,
+                    child: Text(
+                      "Dashboard",
+                      style: AppTypography.titre3.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 85,
+                    right: 15,
+                    child: Container(
+                      width: 60,
+                      height: 60,
+                      decoration: const BoxDecoration(
+                        color: AppColors.primary,
+                        shape: BoxShape.circle,
+                      ),
+                      child: IconButton(
+                        icon: const Icon(
+                          Symbols.settings,
+                          size: 40,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                  Positioned.fill(
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 90),
+                        child: Container(
+                          height: 35,
+                          width: 240,
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(14),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black26,
+                                blurRadius: 8,
+                                offset: Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                          child: Row(
+                            children: [
+                              const Icon(Icons.search, color: Colors.grey),
+                              const SizedBox(width: 10),
+                              Expanded(
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    hintText: "Search tasks...",
+                                    border: InputBorder.none,
+                                    hintStyle: TextStyle(color: Colors.grey),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text("List", style: AppTypography.titre3),
+                    const SizedBox(height: 5),
+                    Container(
+                      padding: const EdgeInsets.all(16),
                       decoration: BoxDecoration(
                         color: Colors.white,
-                        borderRadius: BorderRadius.circular(14),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black26,
-                            blurRadius: 8,
-                            offset: Offset(0, 3),
-                          ),
-                        ],
+                        borderRadius: BorderRadius.circular(12),
                       ),
-                      child: Row(
-                        children: [
-                          const Icon(Icons.search, color: Colors.grey),
-                          const SizedBox(width: 10),
-                          Expanded(
-                            child: TextField(
-                              decoration: InputDecoration(
-                                hintText: "Search tasks...",
-                                border: InputBorder.none,
-                                hintStyle: TextStyle(color: Colors.grey),
-                              ),
-                            ),
-                          ),
-                        ],
+                      child: const Icon(
+                        Symbols.lists_rounded,
+                        size: 50,
+                        color: AppColors.primary,
                       ),
                     ),
-                  ),
+                  ],
                 ),
-              ),
-            ],
-          ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text("Calendar", style: AppTypography.titre3),
+                    const SizedBox(height: 5),
+                    Container(
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: const Icon(
+                        Symbols.calendar_add_on_rounded,
+                        size: 50,
+                        color: AppColors.primary,
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text("Kanban", style: AppTypography.titre3),
+                    const SizedBox(height: 5),
+                    Container(
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: const Icon(
+                        Symbols.view_kanban_rounded,
+                        size: 50,
+                        color: AppColors.primary,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
